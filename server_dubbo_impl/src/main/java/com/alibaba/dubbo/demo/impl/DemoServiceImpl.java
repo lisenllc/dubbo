c8e5b20a1bbacd64a4263b887cbbd128513c68dc
llc <903935523@qq.com>
2015-06-09 17:09:35 +0800

    commit

---
+++ server_dubbo_impl/src/main/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.alibaba.dubbo.demo.impl;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+public class DemoServiceImpl implements DemoService {
+
+	@Override
+	public String sayHello(String name) {
+		// TODO Auto-generated method stub
+		return "Hello " + name;
+	}
+
+}
